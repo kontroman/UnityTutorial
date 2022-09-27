7acbc6edab2b05ea82d80dc141505073bd7929c4
Uncorv <voronoffnr@yandex.ru>
2022-09-27 21:31:54 +0700

    Музыка (пока не работает)

--- Assets/Settings.cs
+++ Assets/Settings.cs
@@ -2,14 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class Settings : MonoBehaviour
 {
+    
     bool isFullScreen;
     public void FullScreenToggle()
     {
        
         isFullScreen = !isFullScreen;
         Screen.fullScreen = isFullScreen;
+    }
+
+
+    public MasterVolume am;
+
+    public void AudioVolume(float sliderValue)
+    {
+    Am.setFloat("masterVolume", sliderValue); 
     }
    
 }
